368b1dcd41a45363e4d745848e2c424f1af8e6f8
Timo Denk <simssosdenk@googlemail.com>
2016-01-09 10:03:30 +0100

    Added flow sensor
    Liquid flow rate sensor

--- Sensor-Arduino/Sensor-Arduino.ino
+++ Sensor-Arduino/Sensor-Arduino.ino
@@ -7,6 +7,10 @@
 #define inputCurrentPin A0
 #define flowCurrrentPin A1
 #define busVoltagePin A2
+
+#define flowSensorInterrupt 0  // 0 = digital pin 2
+#define flowSensorPin 2
+
 #define dataLinkRX 2
 #define dataLinkTX 3
 
@@ -16,6 +20,8 @@ byte * data, * sensorValue;
 
 int dataLength;
 
+unsigned long lastTransmission = 0;
+
 SoftwareSerial dataLink(dataLinkRX, dataLinkTX); // RX, TX
 
 // sensors
@@ -23,6 +29,18 @@ SoftwareSerial dataLink(dataLinkRX, dataLinkTX); // RX, TX
 Adafruit_MCP9808 tempsensor2 = Adafruit_MCP9808();
 Adafruit_MCP9808 tempsensor3 = Adafruit_MCP9808();
 Adafruit_MCP9808 tempsensor4 = Adafruit_MCP9808();*/
+
+
+// flow sensor
+// The hall-effect flow sensor outputs approximately 4.5 pulses per second per
+// litre/minute of flow.
+float calibrationFactor = 4.5;
+volatile byte pulseCount;  
+float flowRate, flowSensorOutput;
+unsigned int flowMilliLitres;
+unsigned long totalMilliLitres;
+unsigned long oldTimeFlowSensor;
+// end flow sensor
 
 void setup() {
   dataLink.begin(1200);
@@ -41,13 +59,66 @@ void setup() {
   tempsensor2.begin(0x19);
   tempsensor3.begin(0x1A);
   tempsensor4.begin(0x1B);*/
+  
+  // flow sensor
+  pinMode(flowSensorPin, INPUT);
+  digitalWrite(flowSensorPin, HIGH);
+
+  pulseCount = 0;
+  flowRate = 0.0;
+  flowMilliLitres = 0;
+  totalMilliLitres = 0;
+  oldTimeFlowSensor = 0;
+
+  // The Hall-effect sensor is connected to pin 2 which uses interrupt 0.
+  // Configured to trigger on a FALLING state change (transition from HIGH
+  // state to LOW state)
+  attachInterrupt(flowSensorInterrupt, pulseCounter, FALLING);
 }
 
 void loop() {
-  if (readSensorData()) { 
+  if ((millis() - lastTransmission) > 1000 && readSensorData()) {
+    lastTransmission = millis();
     // data has changed
     dataLink.write(data, dataLength);
     delay(1000); // interval e.g. 1200ms
+  }
+  
+  if((millis() - oldTimeFlowSensor) > 1000)    // Only process counters once per second
+  { 
+    // Disable the interrupt while calculating flow rate and sending the value to
+    // the host
+    detachInterrupt(flowSensorInterrupt);
+        
+    // Because this loop may not complete in exactly 1 second intervals we calculate
+    // the number of milliseconds that have passed since the last execution and use
+    // that to scale the output. We also apply the calibrationFactor to scale the output
+    // based on the number of pulses per second per units of measure (litres/minute in
+    // this case) coming from the sensor.
+    flowRate = ((1000.0 / (millis() - oldTimeFlowSensor)) * pulseCount) / calibrationFactor;
+    
+    // Note the time this processing pass was executed. Note that because we've
+    // disabled interrupts the millis() function won't actually be incrementing right
+    // at this point, but it will still return the value it was set to just before
+    // interrupts went away.
+    oldTimeFlowSensor = millis();
+    
+    // Divide the flow rate in litres/minute by 60 to determine how many litres have
+    // passed through the sensor in this 1 second interval, then multiply by 1000 to
+    // convert to millilitres.
+    flowMilliLitres = (flowRate / 60) * 1000;
+    
+    // Add the millilitres passed in this second to the cumulative total
+    totalMilliLitres += flowMilliLitres;
+    
+    // sensor value variable
+    flowSensorOutput = (flowRate - int(flowRate)) * 10;
+
+    // Reset the pulse counter so we can start incrementing again
+    pulseCount = 0;
+    
+    // Enable the interrupt again now that we've finished sending output
+    attachInterrupt(flowSensorInterrupt, pulseCounter, FALLING);
   }
 }
 
@@ -88,8 +159,7 @@ boolean readSensorData() {
         break;
       case 7:
         // FlowSensor
-        // TODO: read sensor value
-        sensorValue = 0.0f;
+        sensorValue = flowSensorOutput;
         break;
       case 8:
         // HTU21D-F
@@ -114,4 +184,13 @@ boolean readSensorData() {
   }
   
   return changeDetected;
+}
+
+/*
+Interrupt Service Routine Flow Sensor
+ */
+void pulseCounter()
+{
+  // Increment the pulse counter
+  pulseCount++;
 }
